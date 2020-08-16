6a8d33a3bca1ab4f7096d3964b19099b5a84dc52
Raphael Raja <jrrphonix@gmail.com>
2020-08-16 18:33:38 +0530

    First Simple Skeleton Product

---
+++ src/main/java/com/example/triton/texteditorapp/TexteditorappApplication.java
@@ -0,0 +1,13 @@
+package com.example.triton.texteditorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexteditorappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TexteditorappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/triton/texteditorapp/TexteditorappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.triton.texteditorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TexteditorappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
